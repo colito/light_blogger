f9c529664354556b39d3b1dbb975aad0b15584f2
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-22 12:21:39 +0200

    * modified footer layout

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,6 @@
-<?php
-
-    echo '
-
-                <div id="footer" class="grid_1">
-                    <i>&copy; copyright 2013</i>
-                </div>
-
-            </body>
-        </html>
-
-    ';
-
-?>
+            <div id="footer" class="grid_1">
+                <i>&copy; copyright 2013</i>
+            </div>
+        </div>
+    </body>
+</html>
