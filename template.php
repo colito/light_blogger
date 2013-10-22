87923c60040545d4468ec3ae7adcdb8480b228c4
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-22 10:39:15 +0200

    * added template.php for uniform building

---
+++ template.php
@@ -0,0 +1,8 @@
+<?php
+    $page_name = 'Page Name';
+    require_once('includes/header.php');
+?>
+
+<!-- Place content that would go within the container class -->
+
+<?php require_once('includes/footer.php'); ?>
