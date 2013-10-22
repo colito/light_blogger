d918dc155b05708023cdb0823d1fda932b1130dd
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-22 12:23:01 +0200

    * modified main page according to template

--- index.php
+++ index.php
@@ -1,28 +1,15 @@
 <?php
-   require_once('fns/runner2.php');
-   $blogger = new blogger();
-
    $page_name = 'Home';
-   require_once('includes/head.php');
+   require_once('includes/header.php');
 ?>
 
-<div class="container">
-		
-   <div id="header"class="grid_8">
-        <h2>Main Page</h2>
-   </div>
+<div id="content" class="grid_12">
+    <h3>Welcome!</h3>
+    <p>This is a little demo project to play around with some light weight blogging.</p>
+</div>
 
-   <?php require_once('includes/links.php'); ?>
-
-   <div id="content" class="grid_8">
-       <h3>Welcome!</h3>
-       <p>This is a little demo project to play around with some light weight blogging.</p>
-   </div>
-
-   <div id="blogs" class="grid_12">
-       <?php $blogger->show_all_blogs(); ?>
-   </div>
-
+<div id="blogs" class="grid_12">
+    <?php $blogger->show_all_blogs(); ?>
 </div>
 
 <?php require_once('includes/footer.php'); ?>
