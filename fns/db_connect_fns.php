2e0298c3d44aabb2c8ff33042e3675a031a04283
Pride Mokhele <pride.mokhele@gmail.com>
2013-12-05 12:59:52 +0200

    * updated this file to get it's connection strings from the config file

--- fns/db_connect_fns.php
+++ fns/db_connect_fns.php
@@ -1,8 +1,12 @@
 <?php
-	$hostadress = "localhost";
-	$username = "root";
-	$password = "";
-	$my_sql_db = "bloggers_db";
+    require_once('../config.php');
+    $config = new Config();
+
+	$hostadress = $config->hostadress;
+    echo $hostadress;
+	$username = $config->username;
+	$password = $config->password;
+	$my_sql_db = $config->my_sql_db;
 	
 	$connect = mysql_connect($hostadress, $username, $password);
 	
@@ -12,7 +16,7 @@
 	}
 	else	
 	{
-		//echo "Successful connection";
+		echo "Successful connection";
 		mysql_select_db($my_sql_db, $connect);
 	}
 ?>
